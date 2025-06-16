3d3d0fd21c2a62b8ce9b3a93bc196fe218f16078
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2025-06-16 22:35:34 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,13 +4,10 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const taskRoutes = require('./routes/taskRoutes');
+const taskRoutes = require('./Routes/taskRoutes');
 
 app.use(cors());
 app.use(express.json());
-
-// Routes
 app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
