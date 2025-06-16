1e2a272c4472aad5808c83ff01a06e7619c21500
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2025-06-16 22:25:29 +0530

    Update db.js

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -2,18 +2,20 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectTimeout: 10000
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err);
-        return;
-    }
-    console.log('Connected to MySQL Database');
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("Connected to database");
 });
 
 module.exports = db;
